9cc3f1d262c0d1c1872cca1cebe064f2fa6aafb1
mgh3326 <mgh3326@naver.com>
2018-05-08 14:43:35 +0900

    update

---
+++ chap10.sql
@@ -0,0 +1,15 @@
+use employees;
+# test랑 내 employees랑 같은건가보다
+
+create table t1 (
+  i1 int not null primary key,
+  v2 varchar(20)
+)
+  engine = innodb;
+
+insert into t1 values (1, 'Firebird');
+
+show create table t1;
+
+select *
+from t1;
